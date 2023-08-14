4eca30c85c5cb598588add70d40beb378e84d988
luis hernando <92767376+luisml03@users.noreply.github.com>
2023-08-14 09:21:40 -0500

    Se actualiza el .bat

--- pre-commit.bat
+++ pre-commit.bat
@@ -1,17 +1,28 @@
 @echo off
 
-REM Ruta al repositorio SVN
-
 REM Ruta al ejecutable de Node.js
 set NODE_EXE="C:\Program Files\nodejs\node.exe"
 
-
-set SVNLOOK= "C:\Program Files\TortoiseSVN\bin\svnlook.exe"
 REM Ruta al archivo JavaScript para insertar los datos
 set JS_SCRIPT="C:\repositorio\hooks\take_commit.js"
 
+REM Ruta del back
+set JS_BACK="C:\repositorio\hooks\ventana\backend.js"
+
 REM Ejecutar el script JavaScript para insertar los datos en la base de datos
 %NODE_EXE% %JS_SCRIPT% %1 %2
+
+
+
+REM Ruta al archivo main.js de Electron
+set ELECTRON_APP="C:\repositorio\hooks\ventana\main.js"
+
+REM Cambiar al directorio de tu proyecto de Electron
+cd "C:\repositorio\hooks\ventana"
+
+
+REM Ejecutar el comando npm run start
+npm start
 
 
 REM Verificar el código de salida del script
